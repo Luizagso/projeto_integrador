27760150a255e2726e73508df1566ebc88cdc010
Luiza Garcia <luizaoliveiragso@gmail.com>
2025-09-29 22:52:24 -0300

    :package: build: Instalação de dependências e configurações de projeto

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,32 @@
 // app.js
 const http = require('http');
+const express = require('express');
+const cors = require('cors');
 
-// Configuração usando variáveis de ambiente
-const hostname = process.env.HOST_DATABASE || '127.0.0.1';
-const port = process.env.PORT_NODE || 3000;
+require("dotenv").config()
 
-const server = http.createServer((req, res) => {
+const sequelize = require('./database/database');
+const authController = require('./controller/auth/authController');
+const usuarioController = require('./controller/usuarioController');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/auth', authController);
+app.use('/usuarios', usuarioController);
+
+app.get('/', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Olá Mundo!\n');
+  res.end('Hello World!');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Servidor a rodar em http://${hostname}:${port}/`);
-});
+// Sincronização do modelo com o banco de dados e inicialização do servidor
+sequelize.sync()
+  .then(() => {
+    app.listen(process.env.PORT_NODE, () => {
+      console.log('Servidor rodando em http://localhost:' + process.env.PORT_NODE);
+    });
+  })
+  .catch(err => console.error('Erro ao sincronizar com o banco de dados:', err));
