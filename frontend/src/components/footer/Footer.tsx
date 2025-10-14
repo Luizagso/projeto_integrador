445624048bbbedd17360efb9bf8feeb99b0ef869
marco2299 <marcoaep2020@gmail.com>
2025-10-14 16:13:42 -0300

    feat :sparkles: adiciona footer

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="bg-gray mt-5 d-flex justify-content-center p-3">
+      © 2025 Direitos Reservados | Castor Financeiro
+    </div>
+  );
+}
